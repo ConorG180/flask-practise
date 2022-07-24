3995c5ffacee32c2230c805f9c19cb049d15b944
Conor <conor.ganly@yahoo.com>
2022-07-24 18:34:22 +0000

    Add form with flask and use POST method

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import json
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,8 +10,12 @@ def index():
     return render_template("index.html", page_title="Home")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form)
+        print(request.form.get("email"))
+        
     return render_template("contact.html", page_title="Contact")
 
 
@@ -27,13 +31,12 @@ def about():
 def about_dwarf(dwarve_name):
     with open("data/dwarves.json", "r", encoding="utf-8") as json_data:
         data = json.load(json_data)
-        for member in data:
-            if member["url"] == dwarve_name:
+        for obj in data:
+            if obj["url"] == dwarve_name:
                 # if url of member matches with a dwarve on the list, then 
                 # let declare dwarve and let it = to member
-                dwarve = member
-                return "<h1>" + dwarve["name"] + "</h1>"
-                # return render_template("about_dwarve.html", dwarve)
+                dwarve = obj
+                return render_template("dwarve.html", dwarve=dwarve)
 
 
 @app.route("/careers")
